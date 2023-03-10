8e7b776a8edbb2cb7c457b940ee1d2dc12259e72
Julia-I-P <127091028+Julia-I-P@users.noreply.github.com>
2023-03-10 19:35:34 +0700

    Add files via upload

---
+++ Homework_CSharp/HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Привет!");
+            Console.WriteLine("Ваше имя?");
+            name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+
+        }
+    }
+       
+}
