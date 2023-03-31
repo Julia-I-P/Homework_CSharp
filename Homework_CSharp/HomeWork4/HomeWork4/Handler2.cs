4a4d646ad83cf097f4e1e48630518aec741ada0c
Julia-I-P <Julia.I.Paushkina@gmail.com>
2023-03-31 22:09:49 +0700

    Added HomeWork4.

---
+++ Homework_CSharp/HomeWork4/HomeWork4/Handler2.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork4
+{
+    internal class Handler2
+    {
+        private int _score;
+        public void Message()
+        {
+            Console.WriteLine($"It`s {_score} already! Go and do something!") ;
+        }
+
+        public void SetScore(int score) { _score = score; }
+        public int GetScore() { return _score; }
+    }
+}
